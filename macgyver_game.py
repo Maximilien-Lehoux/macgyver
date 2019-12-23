7244c8a1583056f745e789ec6174498d9f3431e9
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2019-12-23 21:39:51 +0100

    le fond est collé

--- macgyver_game.py
+++ macgyver_game.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 
 pygame.init()
 
-window_height = 300
-window_width = 300
+window_height = 160
+window_width = 520
 
 lost = False
 
@@ -13,14 +13,18 @@ window_size = (window_width, window_height)
 window = pygame.display.set_mode(window_size)
 
 # Stick maze
-background = pygame.image.load("structures.png").convert()
+background = pygame.image.load("pictures/structures.png").convert()
 window.blit(background, (0, 0))
+
+pygame.display.flip()
 
 while not lost:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             lost = True
+
+    window.blit(background, (0, 0))
 
 pygame.display.quit()
 
