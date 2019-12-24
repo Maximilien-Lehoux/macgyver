2e3c150dd1a481fb6a63049a194c9d7d228ca019
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2019-12-24 14:20:20 +0100

    Affichage de macgyver et du gardien

--- macgyver_game.py
+++ macgyver_game.py
@@ -20,10 +20,6 @@ maze = [[140, 0], [160, 0], [180, 0], [200, 0], [220, 0], [240, 0], [280, 0],
 
 
 
-
-
-
-
 lost = False
 
 # window creation
@@ -31,9 +27,25 @@ window_size = (window_width, window_height)
 window = pygame.display.set_mode(window_size)
 
 # Stick maze
-background = pygame.image.load("pictures/structures.png").convert()
+background = pygame.image.load("pictures/structures.png").convert_alpha()
 window.blit(background, (0, 0))
 
+# Stick characters
+character_macgyver = pygame.image.load("pictures/MacGyver.png")
+character_macgyver = pygame.transform.scale(character_macgyver, (20, 20))
+
+character_guardian = pygame.image.load("pictures/Gardien.png").convert_alpha()
+character_guardian = pygame.transform.scale (character_guardian, (20,20))
+
+
+# Create rectangles
+character_macgyver_rect = character_macgyver.get_rect()
+character_macgyver_rect.topleft = (0, 0)
+window.blit(character_macgyver, character_macgyver_rect)
+
+character_guardian_rect = character_guardian.get_rect()
+character_guardian_rect.topleft = (260, 0)
+window.blit(character_guardian, character_guardian_rect)
 
 
 while not lost:
