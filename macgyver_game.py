2e1102168f8fc93908d77b8480fec31d7418adea
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2019-12-24 11:30:27 +0100

    Création du labyrinthe sous forme de liste

--- macgyver_game.py
+++ macgyver_game.py
@@ -4,7 +4,23 @@ from pygame.locals import *
 pygame.init()
 
 window_height = 160
-window_width = 520
+window_width = 300
+
+maze = [[0, 140], [0, 160], [0, 180], [0, 200], [0, 220], [0, 240], [0, 280],
+        [20, 20], [20, 40], [20, 60], [20, 80], [20, 140], [20, 160], [20, 180], [20, 200], [20, 220], [20, 240],
+        [40, 20], [40, 80], [40, 120], [40, 140], [40, 200], [40, 220],
+        [60, 20], [60, 80], [60, 160], [60, 180], [60, 200], [60, 220], [60, 280],
+        [80, 20], [80, 40], [80, 60], [80, 80],
+
+        [120, 60], [120, 80], [120, 120], [120, 140], [120, 160], [120, 180], [120, 220], [120, 240], [120, 260],
+        [120, 280],
+
+        [140, 40], [140, 60], [140, 80], [140, 100], [140, 140], [140, 160], [140, 200], [140, 220], [140, 260],
+        [140, 280]]
+
+
+
+
 
 lost = False
 
@@ -16,7 +32,7 @@ window = pygame.display.set_mode(window_size)
 background = pygame.image.load("pictures/structures.png").convert()
 window.blit(background, (0, 0))
 
-pygame.display.flip()
+
 
 while not lost:
 
@@ -24,7 +40,7 @@ while not lost:
         if event.type == QUIT:
             lost = True
 
-    window.blit(background, (0, 0))
+    pygame.display.flip()
 
 pygame.display.quit()
 
