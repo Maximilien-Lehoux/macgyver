fd4c39f5693437f83617bbbd08d1683e36856b5b
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-01-03 11:30:10 +0100

    Aucun changement

--- macgyver_game.py
+++ macgyver_game.py
@@ -97,7 +97,6 @@ text_game_win = police_game_end.render("YOU WIN !!!", True, pygame.Color("#FFFF0
 square_text_game_win = text_game_win.get_rect()
 square_text_game_win.center = rect_window_end.center
 
-
 while not lost:
     pygame.time.Clock().tick(30)
 
