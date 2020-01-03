e5a182f5edba391acedfe41f9cc46c74de84d3a2
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-01-03 16:47:16 +0100

    Mouvements POO réussi sur le programme entier

--- macgyver_game.py
+++ macgyver_game.py
@@ -43,12 +43,6 @@ background_rules = background_rules.convert()
 background_rules.fill(pygame.Color("#EE3131"))
 window.blit(background_rules, (0, 165))
 
-# create characters and rectangles
-character_macgyver = pygame.image.load("pictures/MacGyver.png").convert_alpha()
-character_macgyver = pygame.transform.scale(character_macgyver, (20, 20))
-character_macgyver_rect = character_macgyver.get_rect()
-character_macgyver_rect.topleft = [0, 0]
-window.blit(character_macgyver, character_macgyver_rect)
 
 character_guardian = pygame.image.load("pictures/Gardien.png").convert_alpha()
 character_guardian = pygame.transform.scale(character_guardian, (20, 20))
@@ -97,6 +91,46 @@ text_game_win = police_game_end.render("YOU WIN !!!", True, pygame.Color("#FFFF0
 square_text_game_win = text_game_win.get_rect()
 square_text_game_win.center = rect_window_end.center
 
+class Characters:
+
+    def __init__(self, picture):
+        self.picture = pygame.image.load(picture).convert_alpha()
+        self.picture = pygame.transform.scale(self.picture, (20, 20))
+        self.rectangle = self.picture.get_rect()
+        self.rectangle.topleft = [0, 0]
+        window.blit(self.picture, self.rectangle)
+
+    def movement_right(self, character):
+        if event.key == K_RIGHT and character.rectangle[0] != window_width - 20:
+            macgyver_location_maze[0] = character.rectangle[0] + 20
+            macgyver_location_maze[1] = character.rectangle[1]
+            if macgyver_location_maze not in maze:
+                character.rectangle = character.rectangle.move(20, 0)
+
+    def movement_left(self, character):
+        if event.key == K_LEFT and character.rectangle[0] != 0:
+            macgyver_location_maze[0] = character.rectangle[0] - 20
+            macgyver_location_maze[1] = character.rectangle[1]
+            if macgyver_location_maze not in maze:
+                character.rectangle = character.rectangle.move(-20, 0)
+
+    def movement_up(self, character):
+        if event.key == K_UP and character.rectangle[1] != 0:
+            macgyver_location_maze[0] = character.rectangle[0]
+            macgyver_location_maze[1] = character.rectangle[1] - 20
+            if macgyver_location_maze not in maze:
+                character.rectangle = character.rectangle.move(0, -20)
+
+    def movement_down(self, character):
+        if event.key == K_DOWN and character.rectangle[1] != 160 - 20:
+            macgyver_location_maze[0] = character.rectangle[0]
+            macgyver_location_maze[1] = character.rectangle[1] + 20
+            if macgyver_location_maze not in maze:
+                character.rectangle = character.rectangle.move(0, 20)
+
+
+character_macgyver = Characters("pictures/MacGyver.png")
+
 while not lost:
     pygame.time.Clock().tick(30)
 
@@ -105,39 +139,24 @@ while not lost:
             lost = True
 
         elif event.type == KEYDOWN:  # and character_macgyver_rect[:2] not in maze:
-            if event.key == K_DOWN and character_macgyver_rect[1] != 160 - 20:
-                macgyver_location_maze[0] = character_macgyver_rect[0]
-                macgyver_location_maze[1] = character_macgyver_rect[1] + 20
-                if macgyver_location_maze not in maze:
-                    character_macgyver_rect = character_macgyver_rect.move(0, 20)
 
-            if event.key == K_RIGHT and character_macgyver_rect[0] != window_width - 20:
-                macgyver_location_maze[0] = character_macgyver_rect[0] + 20
-                macgyver_location_maze[1] = character_macgyver_rect[1]
-                if macgyver_location_maze not in maze:
-                    character_macgyver_rect = character_macgyver_rect.move(20, 0)
+            character_macgyver.movement_right(character_macgyver)
 
-            if event.key == K_LEFT and character_macgyver_rect[0] != 0:
-                macgyver_location_maze[0] = character_macgyver_rect[0] - 20
-                macgyver_location_maze[1] = character_macgyver_rect[1]
-                if macgyver_location_maze not in maze:
-                    character_macgyver_rect = character_macgyver_rect.move(-20, 0)
+            character_macgyver.movement_left(character_macgyver)
 
-            if event.key == K_UP and character_macgyver_rect[1] != 0:
-                macgyver_location_maze[0] = character_macgyver_rect[0]
-                macgyver_location_maze[1] = character_macgyver_rect[1] - 20
-                if macgyver_location_maze not in maze:
-                    character_macgyver_rect = character_macgyver_rect.move(0, -20)
+            character_macgyver.movement_up(character_macgyver)
 
-    if character_macgyver_rect == object_ether_rect:
+            character_macgyver.movement_down(character_macgyver)
+
+    if character_macgyver.rectangle == object_ether_rect:
         object_ether_rect = [40, 175]
         objects_numbers += 1
 
-    if character_macgyver_rect == object_needle_rect:
+    if character_macgyver.rectangle == object_needle_rect:
         object_needle_rect = [240, 175]
         objects_numbers += 1
 
-    if character_macgyver_rect == object_plastic_tube_rect:
+    if character_macgyver.rectangle == object_plastic_tube_rect:
         object_plastic_tube_rect = [140, 175]
         objects_numbers += 1
 
@@ -151,11 +170,11 @@ while not lost:
         window.blit(background_rules, (0, 165))
         window.blit(object_syringe, object_syringe_rect)
 
-    if character_macgyver_rect[:2] == face_guardian:
+    if character_macgyver.rectangle[:2] == face_guardian:
         lost = True
 
     window.blit(character_guardian, character_guardian_rect)
-    window.blit(character_macgyver, character_macgyver_rect)
+    window.blit(character_macgyver.picture, character_macgyver.rectangle)
 
     pygame.display.flip()
 
